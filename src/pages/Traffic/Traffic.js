ac3124cacd7999587a489ed6cbc469dcdfd631a1
NeelSagar39 <neelsagar39@gmail.com>
2021-03-02 11:33:17 +0000

    Done Map

--- src/pages/Traffic/Traffic.js
+++ src/pages/Traffic/Traffic.js
@@ -1,42 +1,59 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Card, Grid } from "@material-ui/core";
-import tt from '@tomtom-international/web-sdk-maps';
-import "./../../assets/ui-library/index.css";
-import "../../maps.css";
+//import tt from '@tomtom-international/web-sdk-maps';
+//import services from '@tomtom-international/web-sdk-services';
+// import "./../../assets/ui-library/index.css";
+// import "../../maps.css";
+import {GoogleMap, useJsApiLoader, DirectionsService, DirectionsRenderer} from '@react-google-maps/api';
+const directionsUrl='https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=place_id:ChIJc-pZtZwOZ0gREjDIGajHACY&destination=place_id:ChIJ3Y7HLZsOZ0gRZ2FxjA3-ACc&mode=driving&key=AIzaSyB9cg4Hwx8RTcNsjQlrhUGS1KwX8pQfqYw';
 export default function Traffic() {
-  useEffect(()=>{ 
-    const script = document.createElement('script');
-    script.src = process.env.PUBLIC_URL + "/sdk/tomtom.min.js";
-    document.body.appendChild(script);
-    script.async = true;
-    script.onload = function () {
-      var map = tt.map({
-          //source: 'vector',
-          key: 'ikSdz6fUg3KdfAz8A7TJtdSHxGDcHP9p',
-          container: 'map',
-          //style: 'tomtom://vector/1/basic-main'
-          stylesVisibility: {
-            trafficIncidents: true,
-            trafficFlow: true
-          }
-        });
-       
-      //map.project(new tt.LngLat.convert([-7.77832031,53.3498]))
-      map.setCenter(new tt.LngLat.convert([-7.77832031,53.3498]));
-      
-       map.flyTo({
-         center:{
-           lng: -6.27,
-           lat: 53.342,
-         },
-         zoom: 12,
+  console.log(DirectionsService)
+  //const [directions,setNewDirections] = useState();
+  const [currentResponse,setCurrentResponse] = useState(null);
+  // useEffect(()=>{
+  //   fetch(directionsUrl).then(res=>res.json()).then((result)=>{
+  //     setNewDirections(result)
+  //     console.log(result);
+  //   },(error)=>{
+  //     console.log(error);
+  //   }
+  //   )
+  // },[])
+useEffect(() => {
+  if (currentResponse) {
+    console.log("Distance & Duration have updated");
+  }
+}, [currentResponse]);
+  const centerMap = {
+    lat: 53.3498,
+    lng: -6.2603
+  };
+  
+  const containerStyle = {
+    width: "100%",
+    height: "100%"
+  };
+  
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDRUrETccsBFX4-M-4hhUsja268r6Rz0AM"
+  })
 
-       });
-      
-      map.addControl(new tt.FullscreenControl());
-      map.addControl(new tt.NavigationControl());
-        
-      }});
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  const updateResponse = result=>{
+    setCurrentResponse(result)
+  }
   const style = {
     height: "95vh",
     margin: "20px",
@@ -85,7 +102,7 @@ export default function Traffic() {
     justifyContent: "center",
   };
   
-  const map={
+  const map_style={
     width: "97%",
     height: "95%",
     padding: "10px",
@@ -97,12 +114,48 @@ export default function Traffic() {
     color: "white",
   }
 
-  return (
+  return isLoaded ? (
     <>
       <Grid container style={style}>
         <Grid item container xs={8} height={1}>
           <Grid item style={gridLeftTop} xs={12}>
-            <Card style={cardLeft} raised><div id = "map" style={map} > </div></Card>
+            <Card style={cardLeft} raised> 
+            <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={{
+          lat: 53.3498,
+          lng: -6.2603
+        }}
+        zoom={13}
+        // onLoad={onLoad}
+        // onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+      
+      <DirectionsService
+                    options={{ 
+                      destination: '53.391176495085,-6.26219900048751',
+                      origin: '53.3525169031735,-6.26392245064604',
+                      travelMode: 'TRANSIT'
+                    }}
+                    // required
+                    callback= {(result)=>{console.log(currentResponse);
+                    if (currentResponse == null){
+                      setCurrentResponse(result);
+                    }}}
+      >
+      </DirectionsService>
+                 
+      {currentResponse!=null&&( 
+        <DirectionsRenderer
+          options={{ 
+                    directions:currentResponse,
+                  }}
+                  
+       > </DirectionsRenderer>
+      )} 
+      </GoogleMap>
+      </Card>
           </Grid>
           <Grid item style={gridLeftBottom} xs={12}>
             <Card style={cardLeft} raised></Card>
@@ -115,5 +168,6 @@ export default function Traffic() {
         </Grid>
       </Grid>
     </>
-  );
+  ) : <>Loading..</>
+
 }
