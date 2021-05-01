b7efdaaf07fdadf9bb81c1f8d3d005dd4f832a76
Vishal Uchil <vishaluchil99@outlook.com>
2021-05-01 11:38:46 +0100

    Air data collection and gauges

--- src/pages/AirQuality/AirQuality.js
+++ src/pages/AirQuality/AirQuality.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect, Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Grid } from "@material-ui/core";
 import "./AirQuality.css";
+import GaugeChart from 'react-gauge-chart'
 import { Line } from "react-chartjs-2";
 
 function AirQuality() {
+
+      const [stations, setStations] = useState([])
 
       const getAllStations = async (lat, lon) => {
             const res = await fetch(`https://api.waqi.info/feed/geo:${lat};${lon}/?token=6bcfce6dc7b7e60a29aefc22f649a96ee0f6ce0d`)
@@ -12,73 +15,59 @@ function AirQuality() {
       }
 
       useEffect(async () => {
-            const api_url = "https://cors-anywhere.herokuapp.com/https://api.waqi.info//map/bounds/?latlng=53.438027,-6.465774,53.271271,-6.093580&token=6bcfce6dc7b7e60a29aefc22f649a96ee0f6ce0d";
+            const api_url = "https://citymanagement.herokuapp.com/pollutiondata";
             const res = await fetch(api_url)
             const data = await res.json()
             const allStations = data.data.map(station => {
                   return [station.lat, station.lon]
             })
-            // console.log(allStations)
             const stationData = []
-            allStations.forEach(station => {
-                  getAllStations(station[0], station[1]).then(response => stationData.push(response))
-            });
-            console.log(stationData)
+            for (const station of allStations) {
+
+                  stationData.push(getCityName(await getAllStations(station[0], station[1])))
+            }
+            // allStations.forEach(async (station) => {
+
+            // });
+            setStations(stationData)
+            // const city = stationData[0].city.name
+            // const onlyCity = city.substr(0, city.indexOf(','))
+            // console.log(onlyCity)
       }, [])
 
-      return (
-            <div>
+      const getCityName = (sta) => {
+            return {
+                  aqi: sta.aqi,
+                  cityName: sta.city.name
+            }
+      }
 
-            </div>
+      return (
+            <>
+                  <Card className="gauge-card" raised>
+                        <h3 style={{ fontSize: "1.7em" }}>Air Quality</h3>
+                        <div className="air-gauge-container">
+                              {stations.map((el) => {
+                                    return (<div key={el.cityName} className="air-gauge" >
+                                          <GaugeChart style={{ width: "90%", fontWeight: 700 }} textColor="#696969" arcWidth={0.3} marginInPercent={0.02} />
+                                          <h3>{el.aqi}</h3>
+                                    </div>)
+                              })}
+                              {/* <div className="air-gauge" style={{ width: "100%" }}>
+                                    <GaugeChart style={{ width: "90%", fontWeight: 700 }} textColor="#696969" arcWidth={0.3} marginInPercent={0.02} />
+                                    <h3>CO2</h3>
+                              </div>
+                              <div className="air-gauge" style={{ width: "100%" }}>
+                                    <GaugeChart style={{ width: "90%", fontWeight: 700 }} textColor="#696969" arcWidth={0.3} marginInPercent={0.02} />
+                                    <h3>CO2</h3>
+                              </div> */}
+                        </div>
+                  </Card>
+                  <Card className="air-chart" raised>
+                        blah blah
+                  </Card>
+            </>
       )
 }
 
 export default AirQuality
-
-
-
-// class AirQuality extends Component {
-//       constructor(props) {
-//             super(props);
-//             this.state = {
-//                   items: []
-//             };
-//       }
-
-//       componentDidMount() {
-//             const apiUrl = 'http://api.openweathermap.org/data/2.5/air_pollution?lat=50&lon=50&appid=87f62a011040d2e6469b4332d4ec4a93';
-
-//             fetch(apiUrl)
-//                   .then((response) => response.json())
-//                   .then((data) =>
-//                         this.setState({ items: data.list[0].components })
-//                   ).catch(console.log);
-//       }
-
-//       render() {
-//             const { items } = this.state;
-
-//             return (
-//                   <Card raised>
-
-//                         <div>
-//                               <h3>Air Pollution Summary</h3>
-//                               <ul>
-//                                     <li>Carbon Dioxide: {items.co}</li>
-//                                     <li>Nitrogen: {items.no}</li>
-//                                     <li>Nitrogen Oxide: {items.no2}</li>
-//                                     <li>Ozone: {items.o3}</li>
-//                                     <li>Sodium Oxide: {items.so2}</li>
-//                                     <li>PM2_5: {items.pm2_5}</li>
-//                                     <li>PM10: {items.pm10}</li>
-//                                     <li>NH3: {items.nh3}</li>
-//                               </ul>
-//                         </div>
-
-//                   </Card>
-
-//             );
-//       }
-// }
-
-// export default AirQuality;
